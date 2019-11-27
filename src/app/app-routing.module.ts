0b485d78064e968954480a12a6d19b3fb4359fdc
Patryk <kluczynski.it@gmail.com>
2019-11-27 15:21:41 +0100

    Rejestracja klienta - wstęp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PasswordResetComponent} from './login/password-reset/password-reset.component';
+import {LoginComponent} from './login/login.component';
+import {ClientFormComponent} from './client-form/client-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'forgetPassword', component: PasswordResetComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: ClientFormComponent  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/client-form/client-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-client-form',
+  templateUrl: './client-form.component.html',
+  styleUrls: ['./client-form.component.css']
+})
+export class ClientFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
