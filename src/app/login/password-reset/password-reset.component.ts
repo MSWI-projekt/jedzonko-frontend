7eda84919558034a94dbb56fc6d4f993c34a1d01
Patryk <kluczynski.it@gmail.com>
2019-11-27 19:34:07 +0100

    Reset hasła - przycisk do strony głównej

--- src/app/login/password-reset/password-reset.component.ts
+++ src/app/login/password-reset/password-reset.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {LoginService} from '../login.service';
+import {PasswordResetService} from './password-reset.service';
 
 @Component({
   selector: 'app-password-reset',
@@ -6,8 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-reset.component.css']
 })
 export class PasswordResetComponent implements OnInit {
+  email: string;
+  constructor(private formBuilder: FormBuilder, private passwordservice: PasswordResetService) {
+  }
+  resetpasswordtoservice() {
+    return this.formBuilder.group({
+      email: ['', Validators.email]
+    });
+  }
 
-  constructor() { }
+  PasswordReset() {
+    this.passwordservice.PasswordReset(this.email).subscribe();
+  }
+
 
   ngOnInit() {
   }
